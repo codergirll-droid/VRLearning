3be40eca4b57e08748456e9fc4203797f2306416
Ayşe Oduncu <59964531+codergirll-droid@users.noreply.github.com>
2022-12-12 00:19:52 +0300

    Project setup

---
+++ Assets/Scripts/HMDInfoManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HMDInfoManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Device name is " + XRSettings.loadedDeviceName);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
